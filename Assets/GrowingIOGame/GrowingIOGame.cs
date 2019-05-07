fc077af120d63a0143c84bd394b59099e87aa699
Lin <growingio@linjies-MacBook-Pro.local>
2019-05-07 17:53:54 +0800

    添加iOS相关资源

--- Assets/GrowingIOGame/GrowingIOGame.cs
+++ Assets/GrowingIOGame/GrowingIOGame.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
 using UnityEngine;
+using System.Runtime.InteropServices;
 
 public class GrowingIOGame {
-    private static string ANDROID_CLASS = "com.growingio.android.plugin.game.GrowingIOGame";
+#if UNITY_ANDROID
 
-    private static AndroidJavaObject dicToMap(Dictionary<string, string> dictionary) {
+        private static string ANDROID_CLASS = "com.growingio.android.plugin.game.GrowingIOGame";
+        private static AndroidJavaObject dicToMap(Dictionary<string, string> dictionary) {
         if (dictionary == null) {
             return null;
         }
@@ -17,61 +19,274 @@ public class GrowingIOGame {
         return map;
     }
 
-    public static void Start(string projectId, string urlScheme) {
-        AndroidJavaObject context = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
-        AndroidJavaClass jc = new AndroidJavaClass(ANDROID_CLASS);
-        jc.CallStatic("start", context, projectId, urlScheme);
-    }
+#endif
+
+#if UNITY_IPHONE
+
+        /* Interface to native implementation */
+
+    [DllImport("__Internal")]
+    private static extern void gioTrack(string eventId);
+
+    [DllImport("__Internal")]
+    private static extern void gioTrackWithNumber(string eventId, double number);
+
+    [DllImport("__Internal")]
+    private static extern void gioTrackWithNumberAndVariable(string eventId, double number, string[] keys, string[] stringValues, int count);
+
+    [DllImport("__Internal")]
+    private static extern void gioTrackWithVariable(string eventId, string[] keys, string[] stringValues, int count);
+
+    [DllImport("__Internal")]
+    private static extern void gioSetEvar(string[] keys, string[] stringValues, int count);
+
+    [DllImport("__Internal")]
+    private static extern void gioSetEvarWithKeyAndString(string key, string stringValue);
+
+    [DllImport("__Internal")]
+    private static extern void gioSetEvarWithKeyAndNumber(string key, double number);
+
+    [DllImport("__Internal")]
+    private static extern void gioSetPeople(string[] keys, string[] stringValues, int count);
+
+    [DllImport("__Internal")]
+    private static extern void gioSetPeopleWithKeyAndString(string key, string stringValue);
+
+    [DllImport("__Internal")]
+    private static extern void gioSetPeopleWithKeyAndNumber(string key, double number);
+
+    [DllImport("__Internal")]
+    private static extern void gioSetVistor(string[] keys, string[] stringValues, int count);
+
+    [DllImport("__Internal")]
+    private static extern void gioSetUserId(string userId);
+
+    [DllImport("__Internal")]
+    private static extern void gioClearUserId();
+
+#endif 
 
     public static void Track(string eventId) {
-        new AndroidJavaClass(ANDROID_CLASS).CallStatic("track", eventId);
+
+        if (Application.platform != RuntimePlatform.OSXEditor && Application.platform != RuntimePlatform.WindowsEditor) {
+            #if UNITY_IPHONE
+                gioTrack(eventId);
+            #endif
+            #if UNITY_ANDROID
+                new AndroidJavaClass(ANDROID_CLASS).CallStatic("track", eventId);
+            #endif
+        } 
+
     }
 
     public static void Track(string eventId, double number) {
-        new AndroidJavaClass(ANDROID_CLASS).CallStatic("track", eventId, number);
+
+        if (Application.platform != RuntimePlatform.OSXEditor && Application.platform != RuntimePlatform.WindowsEditor) {
+        #if UNITY_IPHONE
+            gioTrackWithNumber(eventId, number);
+        #endif
+        #if UNITY_ANDROID
+             new AndroidJavaClass(ANDROID_CLASS).CallStatic("track", eventId, number);
+        #endif
+        }
+
     }
 
     public static void Track(string eventId, Dictionary<string, string> variable) {
-        new AndroidJavaClass(ANDROID_CLASS).CallStatic("track", eventId, dicToMap(variable));
+
+        if (Application.platform != RuntimePlatform.OSXEditor && Application.platform != RuntimePlatform.WindowsEditor) {
+        #if UNITY_IPHONE
+            if (variable != null && variable.Count > 0) {
+                int count = variable.Count;
+                string[] keys = new string[count];
+                string[] stringValues = new string[count];
+                int index = 0;
+                foreach (KeyValuePair<string, string> kvp in variable) {
+                    keys[index] = kvp.Key;
+                    stringValues[index] = (string)kvp.Value;
+                    index++;
+                }
+                gioTrackWithVariable(eventId, keys, stringValues, count);
+            } else {
+                gioTrack(eventId);
+            }
+        #endif
+        #if UNITY_ANDROID
+            new AndroidJavaClass(ANDROID_CLASS).CallStatic("track", eventId, dicToMap(variable));
+        #endif
+        }
+
     }
 
     public static void Track(string eventId, double number, Dictionary<string, string> variable) {
-        new AndroidJavaClass(ANDROID_CLASS).CallStatic("track", eventId, number, dicToMap(variable));
+
+        if (Application.platform != RuntimePlatform.OSXEditor && Application.platform != RuntimePlatform.WindowsEditor) {
+        #if UNITY_IPHONE
+            if (variable != null && variable.Count > 0) {
+                int count = variable.Count;
+                string[] keys = new string[count];
+                string[] stringValues = new string[count];
+                int index = 0;
+                foreach (KeyValuePair<string, string> kvp in variable) {
+                    keys[index] = kvp.Key;
+                    stringValues[index] = (string)kvp.Value;
+                    index++;
+                }
+                gioTrackWithNumberAndVariable(eventId, number, keys, stringValues, count);
+            } else {
+                gioTrackWithNumber(eventId, number);
+            }
+        #endif
+        #if UNITY_ANDROID
+            new AndroidJavaClass(ANDROID_CLASS).CallStatic("track", eventId, number, dicToMap(variable));
+        #endif
+        }
+          
     }
 
     public static void SetEvar(string key, string value) {
-        new AndroidJavaClass(ANDROID_CLASS).CallStatic("setEvar", key, value);
+
+        if (Application.platform != RuntimePlatform.OSXEditor && Application.platform != RuntimePlatform.WindowsEditor) {
+        #if UNITY_IPHONE
+             gioSetEvarWithKeyAndString(key, value);
+        #endif
+        #if UNITY_ANDROID
+             new AndroidJavaClass(ANDROID_CLASS).CallStatic("setEvar", key, value);
+        #endif
+        }
+
     }
 
     public static void SetEvar(string key, double number) {
-        new AndroidJavaClass(ANDROID_CLASS).CallStatic("setEvar", key, number);
+
+        if (Application.platform != RuntimePlatform.OSXEditor && Application.platform != RuntimePlatform.WindowsEditor) {
+        #if UNITY_IPHONE
+            gioSetEvarWithKeyAndNumber(key, number);
+        #endif
+        #if UNITY_ANDROID
+           new AndroidJavaClass(ANDROID_CLASS).CallStatic("setEvar", key, number);
+        #endif
+        }
+        
     }
 
     public static void SetEvar(Dictionary<string, string> variable) {
-        new AndroidJavaClass(ANDROID_CLASS).CallStatic("setEvar", dicToMap(variable));
+
+        if (Application.platform != RuntimePlatform.OSXEditor && Application.platform != RuntimePlatform.WindowsEditor) {
+        #if UNITY_IPHONE
+            if (variable != null && variable.Count > 0) {
+                int count = variable.Count;
+                string[] keys = new string[count];
+                string[] stringValues = new string[count];
+                int index = 0;
+                foreach (KeyValuePair<string, string> kvp in variable) {
+                    keys[index] = kvp.Key;
+                    stringValues[index] = (string)kvp.Value;
+                    index++;
+                }
+                gioSetEvar(keys, stringValues, count);
+            }
+        #endif
+        #if UNITY_ANDROID
+             new AndroidJavaClass(ANDROID_CLASS).CallStatic("setEvar", dicToMap(variable));
+        #endif
+        }
+        
     }
 
     public static void SetPeopleVariable(string key, string value) {
-        new AndroidJavaClass(ANDROID_CLASS).CallStatic("setPeopleVariable", key, value);
+
+        if (Application.platform != RuntimePlatform.OSXEditor && Application.platform != RuntimePlatform.WindowsEditor) {
+        #if UNITY_IPHONE
+            gioSetPeopleWithKeyAndString(key, value);
+        #endif
+        #if UNITY_ANDROID
+            new AndroidJavaClass(ANDROID_CLASS).CallStatic("setPeopleVariable", key, value);
+        #endif
+        }
+
     }
 
     public static void SetPeopleVariable(string key, double number) {
-        new AndroidJavaClass(ANDROID_CLASS).CallStatic("setPeopleVariable", key, number);
+
+        if (Application.platform != RuntimePlatform.OSXEditor && Application.platform != RuntimePlatform.WindowsEditor) {
+        #if UNITY_IPHONE
+            gioSetPeopleWithKeyAndNumber(key, number);
+        #endif
+        #if UNITY_ANDROID
+            new AndroidJavaClass(ANDROID_CLASS).CallStatic("setPeopleVariable", key, number);
+        #endif
+        }
+        
     }
 
     public static void SetPeopleVariable(Dictionary<string, string> variable) {
-        new AndroidJavaClass(ANDROID_CLASS).CallStatic("setPeopleVariable", dicToMap(variable));
+        if (Application.platform != RuntimePlatform.OSXEditor && Application.platform != RuntimePlatform.WindowsEditor) {
+        #if UNITY_IPHONE
+            if (variable != null && variable.Count > 0) {
+                int count = variable.Count;
+                string[] keys = new string[count];
+                string[] stringValues = new string[count];
+                int index = 0;
+                foreach (KeyValuePair<string, string> kvp in variable) {
+                    keys[index] = kvp.Key;
+                    stringValues[index] = (string)kvp.Value;
+                    index++;
+                }
+                 gioSetPeople(keys, stringValues, count);
+            }
+        #endif
+        #if UNITY_ANDROID
+            new AndroidJavaClass(ANDROID_CLASS).CallStatic("setPeopleVariable", dicToMap(variable));
+        #endif
+        }
+        
     }
 
     public static void SetVisitor(Dictionary<string, string> variable) {
-        new AndroidJavaClass(ANDROID_CLASS).CallStatic("setVisitor", dicToMap(variable));
+
+        if (Application.platform != RuntimePlatform.OSXEditor && Application.platform != RuntimePlatform.WindowsEditor) {
+        #if UNITY_IPHONE
+            if (variable != null && variable.Count > 0) {
+                int count = variable.Count;
+                string[] keys = new string[count];
+                string[] stringValues = new string[count];
+                int index = 0;
+                foreach (KeyValuePair<string, string> kvp in variable) {
+                    keys[index] = kvp.Key;
+                    stringValues[index] = (string)kvp.Value;
+                    index++;
+                }
+                gioSetVistor(keys, stringValues, count);
+            }
+        #endif
+        #if UNITY_ANDROID
+            new AndroidJavaClass(ANDROID_CLASS).CallStatic("setVisitor", dicToMap(variable));
+        #endif
+        }
+
     }
 
     public static void SetUserId(string userId) {
-        new AndroidJavaClass(ANDROID_CLASS).CallStatic("setUserId", userId);
+         if (Application.platform != RuntimePlatform.OSXEditor && Application.platform != RuntimePlatform.WindowsEditor) {
+        #if UNITY_IPHONE
+            gioSetUserId(userId);
+        #endif
+        #if UNITY_ANDROID
+            new AndroidJavaClass(ANDROID_CLASS).CallStatic("setUserId", userId);
+        #endif
+        }
+        
     }
 
     public static void ClearUserId() {
-        new AndroidJavaClass(ANDROID_CLASS).CallStatic("clearUserId");
+        if (Application.platform != RuntimePlatform.OSXEditor && Application.platform != RuntimePlatform.WindowsEditor) {
+        #if UNITY_IPHONE
+            gioClearUserId();
+        #endif
+        #if UNITY_ANDROID
+             new AndroidJavaClass(ANDROID_CLASS).CallStatic("clearUserId");
+        #endif
+        }       
     }
 }
---
+++ Assets/Plugins/iOS/GrowingCoreAPI.mm
@@ -0,0 +1,96 @@
+//
+//  GrowingCoreAPI.c
+//  GrowingIO_Manual
+//
+//  Created by GrowingIO on 2019/5/6.
+//  Copyright © 2019 GrowingIO. All rights reserved.
+//
+
+#import <GrowingCoreKit/GrowingCoreKit.h>
+
+// Converts C style string to NSString
+static NSString *gioCreateNSString(const char *string) {
+    if (string)
+        return [NSString stringWithUTF8String:string];
+    else
+        return [NSString stringWithUTF8String:""];
+}
+
+static NSDictionary *gioCreateDiction(const char *keys[], const char *stringValues[], int count) {
+    NSMutableDictionary *dic = [NSMutableDictionary dictionary];
+    for (int i = 0; i < count; i++) {
+        if (keys[i] != NULL && stringValues[i] != NULL) {
+            [dic setObject:gioCreateNSString(stringValues[i])
+                    forKey:gioCreateNSString(keys[i])];
+        }
+    }
+    return dic.copy;
+}
+
+
+extern "C" {
+    
+#pragma GCC diagnostic ignored "-Wmissing-prototypes"
+    
+    //  发送事件
+    void gioTrack(const char *eventId) {
+        [Growing track:gioCreateNSString(eventId)];
+    }
+    
+    void gioTrackWithNumber(const char *eventId, double number) {
+        [Growing track:gioCreateNSString(eventId) withNumber:[NSNumber numberWithDouble:number]];
+    }
+    
+    void gioTrackWithNumberAndVariable(const char *eventId, double number, const char *keys[], const char *stringValues[], int count) {
+        [Growing track:gioCreateNSString(eventId) withNumber:[NSNumber numberWithDouble:number] andVariable:gioCreateDiction(keys, stringValues, count)];
+    }
+    
+    void gioTrackWithVariable(const char *eventId, const char *keys[], const char *stringValues[], int count) {
+        [Growing track:gioCreateNSString(eventId) withVariable:gioCreateDiction(keys, stringValues, count)];
+    }
+    
+    //  转换变量
+    void gioSetEvar(const char *keys[], const char *stringValues[], int count) {
+        [Growing setEvar:gioCreateDiction(keys, stringValues, count)];
+    }
+    
+    void gioSetEvarWithKeyAndString(const char *key, const char *string) {
+        [Growing setEvarWithKey:gioCreateNSString(key) andStringValue:gioCreateNSString(string)];
+    }
+    
+    void gioSetEvarWithKeyAndNumber(const char *key, double number) {
+        [Growing setEvarWithKey:gioCreateNSString(key) andNumberValue:[NSNumber numberWithDouble:number]];
+    }
+    
+    //  用户变量
+    void gioSetPeople(const char *keys[], const char *stringValues[], int count) {
+        [Growing setPeopleVariable:gioCreateDiction(keys, stringValues, count)];
+    }
+    
+    void gioSetPeopleWithKeyAndString(const char *key, const char *string) {
+        [Growing setPeopleVariableWithKey:gioCreateNSString(key) andStringValue:gioCreateNSString(string)];
+    }
+    
+    void gioSetPeopleWithKeyAndNumber(const char *key, double number) {
+        [Growing setPeopleVariableWithKey:gioCreateNSString(key) andNumberValue:[NSNumber numberWithDouble:number]];
+    }
+
+    //  访问用户变量
+    void gioSetVistor(const char *keys[], const char *stringValues[], int count) {
+        [Growing setVisitor:gioCreateDiction(keys, stringValues, count)];
+    }
+    
+    //  设置用户ID
+    void gioSetUserId(const char *userId) {
+        [Growing setUserId:gioCreateNSString(userId)];
+    }
+    
+    //  清除登录用户ID
+    void gioClearUserId(){
+        [Growing clearUserId];
+    }
+    
+#pragma GCC diagnostic warning "-Wmissing-prototypes"
+    
+}
+
